5c1579c51c6e75d9da53f2381d5e7e4f26b7be50
Rafael Alfaro <rafaela@thehangar.cr>
2018-05-27 01:36:29 -0600

    spawns random powerups

--- Assets/Galaxy Shooter/Scripts/SpawnManager.cs
+++ Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -13,6 +13,7 @@ public class SpawnManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (EnemySpawnRoutine ());
+		StartCoroutine (PowerupSpawnRoutine ());
 	}
 	
 	// Update is called once per frame
@@ -21,10 +22,18 @@ public class SpawnManager : MonoBehaviour {
 	}
 
 	//create a coroutine to spawn enemy every 5 seconds
-	private IEnumerator EnemySpawnRoutine(){
+	IEnumerator EnemySpawnRoutine(){
 		//create a game loop (while infinite loop) while true yield return new WaitForSeconds(5)
 		while(true){
 			Instantiate (enemyShipPrefab, new Vector3(Random.Range(-7.0f, 7.0f), 7, 0) , Quaternion.identity );
+			yield return new WaitForSeconds (5.0f);
+		}
+	}
+
+	IEnumerator PowerupSpawnRoutine(){
+		while (true) {
+			int randomPowerup = Random.Range (0, 3);
+			Instantiate (powerups [randomPowerup], new Vector3 (Random.Range (-7.0f, 7.0f), 7, 0), Quaternion.identity);
 			yield return new WaitForSeconds (5.0f);
 		}
 	}
