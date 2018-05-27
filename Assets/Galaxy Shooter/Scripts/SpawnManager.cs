a1ef4f7f1c55341bb64f1db61938be40a08575e9
Rafael Alfaro <rafaela@thehangar.cr>
2018-05-27 00:59:33 -0600

    spawn enemies every 5 seconds

--- Assets/Galaxy Shooter/Scripts/SpawnManager.cs
+++ Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -12,11 +12,20 @@ public class SpawnManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine (SpawnEnemy ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	//create a coroutine to spawn enemy every 5 seconds
+	private IEnumerator SpawnEnemy(){
+		//create a game loop (while infinite loop) while true yield return new WaitForSeconds(5)
+		while(true){
+			yield return new WaitForSeconds (5.0f);
+			Instantiate (enemyShipPrefab, new Vector3(Random.Range(-7.0f, 7.0f), 7) , Quaternion.identity );
+		}
 	}
 }
