9717e1cb0498421fec6620e24ae4f3b697b17637
Rafael Alfaro <rafaela@thehangar.cr>
2018-06-07 21:00:22 -0600

    buttons load corresponding scenes

---
+++ Assets/Galaxy Shooter/Scripts/Main_Menu/Main_Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSinglePlayer(){
+        Debug.Log("Single Player Loading...");
+        //Load Scene 1 -- single player scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCoOp()
+    {
+        Debug.Log("Co-Op Loading...");
+        //Load Scene 2 -- Co-Op scene
+        SceneManager.LoadScene(2);
+    }
+}
